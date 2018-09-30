260d214120f27e4648caf15bb2c1b75bf7b27a97
allera <llera.a1@gmail.com>
2018-09-30 13:45:07 +0200

    Add files via upload

---
+++ Spectral_reordering/matlab/example/Spectral_reordering_details_example.m
@@ -0,0 +1,100 @@
+% based on: Changes in connectivity profiles define functionally desitinct regions in
+%human medial frontal cortex.PNAS
+clear
+
+
+%generate data
+    
+    %I generate timeseries (N voxels, t timesteps)
+        N=20; % even!, corr matrix of size NxN
+        t=100;% time samples
+
+    %all voxels are dependent in two signals
+        signal1=randn(1,t);
+        signal2=randn(1,t);
+
+    %half of voxels dependent on each of the two signals
+    % and lambda is the dependence factor.
+        m=N/2;
+        lambda=0.8;
+        
+    %I put data in x
+
+        x(1,1:t)=signal1;
+        for i=2:m
+            x(i,:)=(1-lambda).* randn(1,t)+ (lambda .*signal1);
+        end
+
+
+        x(m+1,1:t)=signal2;
+        for i=m+2:2*m
+            x(i,:)=(1-lambda).* randn(1,t)+ (lambda .* signal2);
+        end
+        
+    %I call it data
+
+    data=x';
+
+
+%compute correlation
+    corrMatrix=corr(data); % we want to recover this ordered structure
+
+
+%FROM HERE ON VARIABLES ARE CALLED EXACTLY AS IN THE SUPPROTING TEXT OF THE
+%PAPER
+
+%mix it 
+    mix=randperm(N);
+    
+    B=corrMatrix(mix,:);
+    B=B(:,mix);
+
+%add one to make all postive entries
+    C=B+1;
+
+%compute Q
+    Q=-C;%init Q
+    Q=Q-diag(diag(Q));%zeros in diagonal
+    tmp=sum(Q);
+    Q=Q-diag(tmp);
+
+%compute t
+    tmp2=1./sqrt(sum(C));
+    t=diag(tmp2);
+
+%compute D
+    D=t*Q*t;
+
+%eigevalue decomposition
+    [W D]=eig(D);
+    v=W(:,2);
+
+%scale v
+    v2=t*v;
+
+%find the searched permutation p
+    [orderedv2 permutation]=sort(v2,'ascend')
+
+% Reorder B ;)
+    orderedB=B(permutation,:);
+    orderedB=orderedB(:,permutation);
+    
+    
+%Make a plot
+
+figure(1);clf
+
+subplot(3,1,1)
+imagesc(corrMatrix)
+title('Original Pattern')
+
+subplot(3,1,2)
+imagesc(B)
+title('Randomized Original Pattern')
+
+
+subplot(3,1,3)
+imagesc(orderedB)
+title('Reordered Pattern')
+
+
